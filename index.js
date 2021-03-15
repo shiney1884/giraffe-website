f858b721746436927089318bc96812ec7e63fe45
shiney1884 <scott0406@outlook.com>
2021-03-15 12:57:04 +0000

    desktop navigation started

--- index.js
+++ index.js
@@ -7,85 +7,113 @@ let searchQuery = 'Pencil';
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
 
-app.listen(3000, ()=> {
+app.listen(3000, () => {
     console.log('server started on port 3000');
 });
 
-app.get('/', (req, res)=> {
+app.get('/', (req, res) => {
     let title = 'Home | Giraffe Website';
-    res.render('index', {title: title});
+    res.render('index', {
+        title: title
+    });
 })
 
-app.get(`/${clickedProduct}`, (req, res)=> {
+app.get(`/${clickedProduct}`, (req, res) => {
     let title = `${clickedProduct} | Giraffe Website`;
     let header = `${clickedProduct}`;
-    res.render('products', {title: title, header: header});
+    res.render('products', {
+        title: title,
+        header: header
+    });
 });
 
-app.get('/pens', (req, res)=> {
+app.get('/pens', (req, res) => {
     let title = 'Pens | Giraffe Website';
     let header = 'Pens';
-    res.render('products', {title: title, header: header});
+    res.render('products', {
+        title: title,
+        header: header
+    });
 });
 
-app.get('/artcontest', (req, res)=> {
+app.get('/artcontest', (req, res) => {
     let title = 'Art Contest | Giraffe Website';
     let header = 'Art Contest';
-    res.render('contest', {title: title, header: header});
+    res.render('contest', {
+        title: title,
+        header: header
+    });
 });
 
 
-app.get('/contact', (req, res)=> {
+app.get('/contact', (req, res) => {
     let title = 'Contact | Giraffe Website';
     let header = 'Contact Us';
-    res.render('contact', {title: title, header: header});
+    res.render('contact', {
+        title: title,
+        header: header
+    });
 });
 
-app.get('/createaccount', (req, res)=> {
+app.get('/createaccount', (req, res) => {
     let title = 'Create an Account | Giraffe Website';
-    res.render('createaccount', {title: title});
+    res.render('createaccount', {
+        title: title
+    });
 });
 
-app.get('/login', (req, res)=> {
+app.get('/login', (req, res) => {
     let title = 'Login | Giraffe Website';
-    res.render('login', {title: title});
+    res.render('login', {
+        title: title
+    });
 });
 
-app.get('/basket', (req, res)=> {
+app.get('/basket', (req, res) => {
     let title = 'Your Basket | Giraffe Website';
-    res.render('basket', {title: title});
+    res.render('basket', {
+        title: title
+    });
 });
 
-app.get('/checkout', (req, res)=> {
+app.get('/checkout', (req, res) => {
     let title = 'Checkout | Giraffe Website';
-    res.render('checkout', {title: title});
+    res.render('checkout', {
+        title: title
+    });
 });
 
 
-app.get('/youraccount', (req, res)=> {
+app.get('/youraccount', (req, res) => {
     let title = 'Your Account | Giraffe Website';
     let header = 'Your Account';
-    res.render('youraccount', {title: title, header: header});
+    res.render('youraccount', {
+        title: title,
+        header: header
+    });
 });
 
-app.get('/orders', (req, res)=> {
+app.get('/orders', (req, res) => {
     let title = 'Your Orders | Giraffe Website';
-    res.render('orders', {title: title});
+    res.render('orders', {
+        title: title
+    });
 });
 
-app.get(`/${searchQuery}`, (req, res)=> {
+app.get(`/${searchQuery}`, (req, res) => {
     let title = 'Your Search | Giraffe Website';
     let header = 'Search Results';
-    res.render('products', {title: title, header: header});
+    res.render('products', {
+        title: title,
+        header: header
+    });
 });
 
-app.get('/wishlist', (req, res)=> {
+app.get('/wishlist', (req, res) => {
     let title = 'Your Wishlist | Giraffe Website';
     let header = 'Wishlist';
-    res.render('wishlist', {title: title, header: header});
+    res.render('wishlist', {
+        title: title,
+        header: header
+    });
 });
-
-
-
-
-
