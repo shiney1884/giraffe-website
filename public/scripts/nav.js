31452f632dbcca9f358f7ea3607c17c692d52323
shiney1884 <scott0406@outlook.com>
2021-03-17 15:45:48 +0000

    hamburger icon functionality complete

--- public/scripts/nav.js
+++ public/scripts/nav.js
@@ -1,1 +1,14 @@
-console.log('connected js file');
+const burger = document.getElementById('burger');
+const links = document.getElementById('links');
+const signInLink = document.getElementById('sign-in-link');
+const burgerSpan1 = document.getElementById('first-bar');
+const burgerSpan2 = document.getElementById('second-bar');
+const burgerSpan3 = document.getElementById('third-bar');
+
+burger.addEventListener('click', ()=> {
+    links.classList.toggle('down');
+    signInLink.classList.toggle('down');
+    burgerSpan1.classList.toggle('first-bar');
+    burgerSpan2.classList.toggle('second-bar');
+    burgerSpan3.classList.toggle('third-bar');
+})
